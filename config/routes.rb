c999d3ac8a871413b2962740e65e51979b0a455a
J. Antonio Salomon <josesalomon@gmail.com>
2017-04-26 21:05:49 -0500

    First Upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#create
+get("/photos/new", {:controller => "photos", :action => "new_form"})
+get("/create_photo", {:controller => "photos", :action => "create_row"})
+
+#Read
+get("/photos", {:controller => "photos", :action => "index"})
+get("/photos/:id", {:controller => "photos", :action => "show"})
+
+#Update
+get("/photos/:id/edit", {:controller => "photos", :action => "edit_form"})
+get("/update_photo/:id", {:controller => "photos", :action => "update_row"})
+
+#Delete
+get("/delete_photos/:id", {:controller => "photos", :action => "destroy_form"})
+
+
 end
